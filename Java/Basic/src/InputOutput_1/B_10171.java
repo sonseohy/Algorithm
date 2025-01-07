ef1910a270281f0540b101fbf90c034c92e64632
sonseohy <sonseoh223@gmail.com>
2025-01-08 00:22:03 +0900

    java: input output

---
+++ Java/Basic/src/InputOutput_1/B_10171.java
@@ -0,0 +1,12 @@
+package InputOutput_1;
+
+// 고양이
+// 예제와 같이 고양이를 출력하시오.
+public class B_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
---
+++ Java/Basic/src/InputOutput_1/B_2557.java
@@ -0,0 +1,9 @@
+package InputOutput_1;
+
+// Hello World
+// Hello World!를 출력하시오.
+public class B_2557 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
